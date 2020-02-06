b226d1addec691eb6903d1c18415ddd42af833b3
Jordan Banasik <woovie@woovie.net>
2020-02-06 16:42:19 -0600

    Complete rewrite
    Logic simplication
    Code simplication
    Rename a lot of variables

--- main.py
+++ main.py
@@ -1,56 +1,75 @@
-import datetime, discord, json, twitch, configparser
+import aiohttp, configparser, discord
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-streamer_objs = []
+streamer_api = config['twitch']['streamer_api']
+game_api     = config['twitch']['game_api']
+client_id    = config['twitch']['clientid']
+headers      = {'Client-ID': client_id}
 
 class DiscordClient(discord.Client):
     async def on_ready(self):
         print(f"Logged in as {self.user}")
 
-async def check_streamer(streamer):
-    print(f"Checking {streamer.streamer}")
-    streamer_data = await streamer.get_streamer_data()
-    if len(streamer_data['data']) > 0:
-        print(f"Twitch returned data for {streamer.streamer}")
-        now_time   = int(datetime.datetime.now().strftime('%s'))
-        start_time = int(datetime.datetime.strptime(streamer_data['data'][0]['started_at'], "%Y-%m-%dT%H:%M:%SZ").strftime('%s'))
-        print(f"Now: {now_time}, {streamer.streamer} Started: {start_time}")
-        if (now_time-start_time) < 600:
-            live_check = await streamer.live_check(streamer_data)
-            print(f"{streamer.streamer} started less than 600sec ago, checking if announced")
-            if live_check == 'live':
-                print(f"First check, announcing for {streamer.streamer}")
-                game_data = await twitch.get_game_data_obj(streamer_data)
-                embed = discord.Embed(title=f"https://twitch.tv/{streamer.streamer}", color=discord.Color(0x6441a4))
-                embed.set_footer(text="bot by Woovie#5555 | https://github.com/woovie")
-                embed.set_thumbnail(url="https://assets.help.twitch.tv/Glitch_Purple_RGB.png")
-                embed.set_image(url=game_data['art'])
-                embed.add_field(name='Title', inline=False, value=streamer_data['data'][0]['title'])
-                embed.add_field(name='Category', inline=False, value=game_data['name'])
-                channel = discord_client.get_channel(int(streamer.announce_channel))
-                await channel.send(content=streamer.message, embed=embed)
+class Streamer():
+    def __init__(self, streamer, frequency, message, channel):
+        self.streamer = streamer
+        self.went_live = 0
+        self.live_last_tick = False
+        self.message = message
+        self.channel = channel
+        self.job = sched.add_job(self.process_live_check, 'interval', minutes=frequency)
+        self.embed = discord.Embed(title=f"https://twitch.tv/{streamer.streamer}", color=discord.Color(0x6441a4))
+        self.embed.set_footer(text="bot by Woovie#5555 | https://github.com/woovie")
+        self.embed.set_thumbnail(url="https://assets.help.twitch.tv/Glitch_Purple_RGB.png")
+    def async process_live_check(self):
+        if (live_data := await self.is_live()) is not None:
+            if not live_last_tick:
+                now = int(datetime.datetime.now().strftime('%s'))
+                if now - self.went_live > 600:
+                    self.went_live = int(datetime.datetime.strptime(live_data[0]['started_at'], "%Y-%m-%dT%H:%M:%SZ").strftime('%s'))
+                    self.live_last_tick = True
+                    game_data = await self.get_game_data(self, live_data[0]['game_id'])
+                    self.embed.add_field(name='Title', inline=False, value=live_data[0]['title'])
+                    self.embed.add_field(name='Category', inline=False, value=game_data['name'])
+                    self.embed.set_image(url=game_data['art'])
+                    channel = discord_client.get_channel(int(self.channel))
+                    channel.send(content=self.message, embed=self.embed)
+                else:
+                    self.live_last_tick = True
             else:
-                print(f"Live checker message was: {live_check}")
+                live_last_tick = True
         else:
-            print(f"{streamer.streamer} started greater than 600 sec ago, no further checks.")
-    else:
-        print(f"No twitch data for {streamer.streamer}")
+            self.live_last_tick = False
+    def async is_live(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{streamer_api}{self.streamer}", headers=headers) as r:
+                if r.status == 200:
+                    live_data = await r.json()['data']
+                    if len(live_data) > 0:
+                       return live_data
+                    else:
+                        return None
+                else:
+                    return None
+    def async get_game_data(self, game_id):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{game_api}{game_id}", headers=headers) as r:
+                if r.status == 200:
+                    game_data = await r.json()
+                    return {
+                        'name': game_data['data'][0]['name'],
+                        'art': game_data['data'][0]['box_art_url'].format(width="170", height="226")
+                    }
 
-def main(discord_client):
-    streamers = None
-    with open('streamers.json', 'r') as f:
-        streamers = json.load(f)
-    for streamer in streamers:
-        if streamer['announce']:
-            print(f"Adding {streamer['streamer']}")
-            streamer_obj = twitch.Streamer(streamer['streamer'], streamer['speed'], streamer['announce'], streamer['announce_channel'], streamer['message'], discord_client)
-            sched.add_job(check_streamer, 'interval', args=[streamer_obj], minutes=streamer_obj.check_freq)
-    sched.start()
-
-sched = AsyncIOScheduler()
-discord_client = DiscordClient()
-main(discord_client)
 discord_client.run(config['discord']['token'])
+sched = AsyncIOScheduler()
+with open('streamers.json', 'r') as f:
+    streamers = json.load(f)
+    for streamer in streamers:
+        if streamer['active']:
+                print(f"Adding streamer {streamer['twitch_name']}")
+                Streamer(streamer['twitch_name'], streamer['frequency'], streamer['message'], streamer['announce_channel'])
+sched.start()
